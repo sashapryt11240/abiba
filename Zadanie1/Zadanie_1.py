0ff8e82f623839b595a8c45656365ba0a807e007
sashapryt11240 <sashapryt11240@gmail.com>
2023-12-12 12:28:01 +0100

    first commit

--- Zadanie1/Zadanie_1.py
+++
@@ -1,9 +0,0 @@
-print('Hello, Aboba!')
-#print('Hello, Aboba!'), виводить на екран Hello, Aboba!
-print('Hello, World!')
-#print('Hello, World!'), виводить на екран Hello, World!
-Abiba = print('Hello, Abiba!')
-Abiba = print('Hello, Bimba!')
-
-#[ ]Закінчити всі завдання
-#[ ]Вчити математику
