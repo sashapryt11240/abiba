ae0dc97885ce9b5c3325dbf06855f8607111215b
sashapryt11240 <sashapryt11240@gmail.com>
2023-12-12 12:16:44 +0100

    Перший комiт

---
+++ Zadanie1/Zadanie_1.py
@@ -0,0 +1,9 @@
+print('Hello, Aboba!')
+#print('Hello, Aboba!'), виводить на екран Hello, Aboba!
+print('Hello, World!')
+#print('Hello, World!'), виводить на екран Hello, World!
+Abiba = print('Hello, Abiba!')
+Abiba = print('Hello, Bimba!')
+
+#[ ]Закінчити всі завдання
+#[ ]Вчити математику
---
+++ Zadanie1/project.py
@@ -0,0 +1,4 @@
+print('Hello, Aboba!')
+#print('Hello, Aboba!'), виводить на екран Hello, Aboba!
+print('Hello, World!')
+#print('Hello, World!'), виводить на екран Hello, World!
